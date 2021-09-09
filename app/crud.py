6bc1fbdf7ec65231de2ae730b58e50ccff49d81c
Ben Kettle <theyellowtoaster@gmail.com>
2021-09-08 22:07:31 -0400

    sort photos descending on read

--- app/crud.py
+++ app/crud.py
@@ -5,12 +5,12 @@ from . import models, schemas
 
 def get_photos(db: Session, skip: int = 0, limit: int = 100) -> List[models.Photo]:
     # offset and limit allow for pagination
-    return db.query(models.Photo).offset(skip).limit(limit).all()
+    return db.query(models.Photo).order_by(models.Photo.id.desc()).offset(skip).limit(limit).all()
 
 
 def get_photos_by_issue(db: Session, volume: int, issue: int) -> List[models.Photo]:
     return db.query(models.Photo).filter(models.Photo.volume == volume)\
-        .filter(models.Photo.issue == issue).all() 
+        .filter(models.Photo.issue == issue).order_by(models.Photo.id.desc()).all() 
 
 
 def create_photo(db: Session, photo: schemas.PhotoCreate) -> models.Photo:
